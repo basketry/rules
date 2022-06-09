4bd2a83b1af9a334ab5b6358a55286b7456d2c93
Steve Konves <stephen.konves@gmail.com>
2022-06-09 06:09:45 -0700

    feat: restrict to specific verbs

--- src/offset-pagination.ts
+++ src/offset-pagination.ts
@@ -3,7 +3,17 @@ import { snake } from 'case';
 import { isArrayPayload, parseSeverity } from './utils';
 
 const offsetPaginationRule = methodRule(
-  ({ method, service, sourcePath, options }) => {
+  ({ method, httpMethod, service, sourcePath, options }) => {
+    const verbs = options?.verb || options?.verbs || 'get';
+    const allow =
+      !httpMethod?.verb ||
+      `${verbs}`.toLowerCase() === httpMethod.verb.value ||
+      (Array.isArray(verbs) &&
+        verbs
+          .map((verb) => `${verb}`.toLowerCase())
+          .includes(httpMethod.verb.value));
+
+    if (!allow) return;
     if (!isArrayPayload(service, options, method.returnType)) return;
 
     const offset = method.parameters.find(
